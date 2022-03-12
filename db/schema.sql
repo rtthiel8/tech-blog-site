9a8515a49c4a953606450931b569847fe566a6b5
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-03-11 19:29:11 -0600

    initial setup/user model

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS tech_blog_site_db;
+
+CREATE DATABASE tech_blog_site_db;
+
---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
